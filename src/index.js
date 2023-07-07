70e42dd5bacc448fc22184b9dee9132a99b37f40
Dragos Dumitras <dragos.dumitras@linnify.com>
2023-07-07 12:07:09 +0300

    load and render teams

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./style.css";
 
 console.warn("start team");
+
+function getTeamsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>x</td>
+  </tr>`;
+}
+
+function renderTeams(teams) {
+  const htmlTeams = teams.map(getTeamsHTML);
+  document.querySelector("#teamsTable tbody").innerHTML = htmlTeams.join("");
+}
+
+function loadTeams() {
+  fetch("teams.json")
+    .then((r) => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
