862c5b004766b774f51f0f26bfd890b50fe436b0
Dragos Dumitras <dragos.dumitras@linnify.com>
2023-07-14 18:31:34 +0300

    search in all fields

--- src/index.js
+++ src/index.js
@@ -187,9 +187,15 @@ function setInputsDisabled(disabled) {
 }
 
 function filterElements(teams, search) {
+  search = search.toLowerCase();
   return teams.filter((team) => {
     // console.info("search %o in %o", search, team.promotion);
-    return team.promotion.includes(search);
+    return (
+      team.promotion.toLowerCase().includes(search) ||
+      team.members.toLowerCase().includes(search) ||
+      team.name.toLowerCase().includes(search) ||
+      team.url.toLowerCase().includes(search)
+    );
   });
 }
 
